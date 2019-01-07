405ec32b0bdb1f4694e36a51c497ac4b0ba3c42a
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-01-07 19:33:35 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+usr_1=int(raw_input())
+usr_2=int(raw_input())
+usr_3=int(raw_input())
+if (usr_1 > usr_2) and (usr_1 > usr_3):
+	print usr_1
+elif (usr_2>usr_1) and (usr_2>usr_3):
+	print usr_2
+else:
+	print usr_3
